c12fc3a77197105e2140536da28b0a970b774473
Varnit <varnitshukla111@gmail.com>
2025-03-26 13:45:44 +0530

    Solution

--- 9.palindrome-number.java
+++ 9.palindrome-number.java
@@ -1,6 +1,12 @@
+/*
+ * @lc app=leetcode id=9 lang=java
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
 class Solution {
     public boolean isPalindrome(int x) {
-        // Negative numbers and numbers ending in 0 (except 0 itself) are not palindromes
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
@@ -17,3 +23,5 @@ class Solution {
         return original == reversed;
     }
 }
+// @lc code=end
+
