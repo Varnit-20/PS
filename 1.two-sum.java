3053726d7b105e3aca9e30183d963d1dc06da589
Varnit <varnitshukla111@gmail.com>
2025-03-27 02:25:32 +0530

    initial code late night 2:25 will complete later

---
+++ 1.two-sum.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        for(int i = 0 ; i <= nums.length; i++ ){
+            for(int j = nums.length-1 ; j >= 0 ; j--){
+                if(nums[i] + nums[j]== target){
+                    int ansp[] 
+                }
+            }
+        }
+        
+    }
+}
+// @lc code=end
+
